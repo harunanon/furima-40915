00e2ab12e386c690e43531ded65c76f6a87a02e3
harunanon <haruna.nobuoka@gmail.com>
2024-05-23 16:46:17 +0900

    ユーザー管理機能の実装

---
+++ app/controllers/users/registrations_controller.rb
@@ -0,0 +1,10 @@
+class Users::RegistrationsController < Devise::RegistrationsController
+  before_action :configure_sign_up_params, only: [:create]
+
+  protected
+
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :email, :password, :password_confirmation, :first_name, :last_name, :first_name_kana, :last_name_kana, :birth_date])
+  end
+ 
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   get 'items/index'
   root 'items#index'
-  resources :items
+  resources :items 
+
 end
