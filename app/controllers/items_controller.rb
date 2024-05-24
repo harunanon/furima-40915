f0f8654edde340f70f42b06e25dfa2cea4ae466f
harunanon <haruna.nobuoka@gmail.com>
2024-05-24 15:18:36 +0900

    出品するボタンの実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,4 +2,8 @@ class ItemsController < ApplicationController
   def index
     # @items = Item.all # 例として、全てのItemを取得
   end
+
+  def new
+    @item = Item.new
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_05_22_030616) do
+ActiveRecord::Schema[7.0].define(version: 2024_05_24_025156) do
+  create_table "items", charset: "utf8", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.integer "price"
+    t.string "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", charset: "utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
